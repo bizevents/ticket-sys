a2b18ca9069e279ec18e00049028ac7727570664
bizevents <bizbazevents@gmail.com>
2024-12-07 13:44:35 +0300

    database connection

--- server/scripts/generateTickets.js
+++ server/scripts/generateTickets.js
@@ -1,15 +1,16 @@
 const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config(); // Load environment variables from .env file
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  host: 'bizbazevents-bizbazevents-03a7.l.aivencloud.com', // Replace with your Aiven MySQL host
-  port: 27994 , // Replace with the correct port if it's different
-  username: 'avnadmin', // Replace with your Aiven database username
-  password: 'AVNS_tIapblDP65gM2SIOmds', // Replace with your Aiven database password
-  database: 'defaultdb', // Replace with your Aiven database name
+  host: process.env.DB_HOST, // Aiven MySQL host
+  port: process.env.DB_PORT, // Aiven MySQL port
+  username: process.env.DB_USER, // Aiven database username
+  password: process.env.DB_PASSWORD, // Aiven database password
+  database: process.env.DB_NAME, // Aiven database name
   dialectOptions: {
-    connectTimeout: 10000, // Optional: Adjust the connection timeout (in milliseconds)
+    connectTimeout: 10000, // Connection timeout (in milliseconds)
   },
-  logging: false, // Optional: Set to true to log SQL queries
+  logging: process.env.DB_LOGGING === 'true', // Enable logging if DB_LOGGING=true in .env
 });
 // Import the Ticket model here
 const Ticket = require('../models/Ticket'); 
